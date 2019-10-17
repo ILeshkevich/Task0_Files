b18342ba27a4c9239f1925d275e290b4366cff3b
i.leshkevich <i.leshkevich@itransition.com>
2019-10-17 16:24:45 +0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using LibGit2Sharp;
+using System.IO;
+using GithubFiles.utils;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GithubFiles
+{
+    class Program
+    {
+        private static readonly List<string> filesChanges = new List<string>();
+        private const string login = "fileTracker";
+        private const string pass = "trackerpass1";
+        private static string path = Environment.CurrentDirectory + "\\Repo";
+        static void Main(string[] args)
+        {
+            DeleteDirectory(path);
+            Console.Clear();
+            Console.WriteLine("Input reository url \nFor example(https://github.com/ILeshkevuch/test.git)");
+            var url = Console.ReadLine();
+
+            Git.Clone(url,login,pass,path);
+            Git.Log(path, filesChanges);
+
+            var files = filesChanges.GroupBy(fC => fC).Select(f => new { Key = f.Key, Count = f.Count() }).ToList();
+            files = files.OrderByDescending(f => f.Count).ToList();
+            for (int i = 0; i < (files.Count>10?10:files.Count); i++)
+            {
+                Console.WriteLine($"{i+1}:{files[i].Key} -- {files[i].Count}");
+            }
+            DeleteDirectory(path);
+        }
+
+        public static void DeleteDirectory(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                return;
+            }
+
+            var files = Directory.GetFiles(directoryPath);
+            var directories = Directory.GetDirectories(directoryPath);
+
+            foreach (var file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (var dir in directories)
+            {
+                DeleteDirectory(dir);
+            }
+
+            File.SetAttributes(directoryPath, FileAttributes.Normal);
+
+            Directory.Delete(directoryPath,false);
+        }
+    }
+}
---
+++ utils/git.cs
@@ -0,0 +1,49 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubFiles.utils
+{
+    class Git
+    {
+        static public void Clone(string url, string login, string pass, string path)
+        {
+            try
+            {
+                var co = new CloneOptions();
+                co.CredentialsProvider = (_url, _user, _cred) => new UsernamePasswordCredentials { Username = login, Password = pass };
+                Console.WriteLine("Cloning...");
+                Repository.Clone(url, path, co);
+                Console.WriteLine("Comlite");
+            }
+            catch (Exception e)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ResetColor();
+            }
+        }
+
+
+        static public void Log(string path, List<string> files)
+        {
+            using (var repo = new Repository(path))
+            {
+                foreach (var commit in repo.Commits)
+                {
+                    foreach (var parent in commit.Parents)
+                    {
+                        foreach (TreeEntryChanges change in repo.Diff.Compare<TreeChanges>(parent.Tree, commit.Tree))
+                        {
+                            files.Add(change.Path);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
